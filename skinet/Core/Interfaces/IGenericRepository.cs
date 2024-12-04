826dfabdb0a717220b5e903944f72178c107e5ba
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-12-04 06:13:23 +0200

    4.34. Updating the repository to use the specification

--- skinet/Core/Interfaces/IGenericRepository.cs
+++ skinet/Core/Interfaces/IGenericRepository.cs
@@ -2,11 +2,13 @@ namespace Core.Interfaces;
 
 public interface IGenericRepository<T> where T : BaseEntity
 {
-    Task<T?> GetByIdAsync(int id);
-    Task<IReadOnlyList<T>> GetAllAsync();
-    void Add(T entity);
-    void Update(T entity);
-    void Delete(T entity);
-    Task<bool> SaveAllAsync();
     Task<bool> ExistsAsync(int id);
+    Task<bool> SaveAllAsync();
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    Task<T?> GetByIdAsync(int id);
+    Task<T?> GetEntityWithSpec(ISpecification<T> spec);
+    void Add(T entity);
+    void Delete(T entity);
+    void Update(T entity);
 }
