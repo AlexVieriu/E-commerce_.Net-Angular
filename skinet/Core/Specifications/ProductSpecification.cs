7b92a62dd4d00db17571a83fa8b18c6b31cf0f9b
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-12-05 10:38:46 +0200

    5.48. Adding the search functionality

--- skinet/Core/Specifications/ProductSpecification.cs
+++ skinet/Core/Specifications/ProductSpecification.cs
@@ -4,6 +4,7 @@ public class ProductSpecification : BaseSpecification<Product>
 {
     // if there are no filters(==0), or contain filers, apply OrderBy
     public ProductSpecification(ProductSpecParams specParams) : base(x =>
+        (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
         (specParams.Brands.Count == 0 || specParams.Brands.Contains(x.Brand)) &&
         (specParams.Types.Count == 0 || specParams.Types.Contains(x.Type)))
     {
