c28fa3802351fb3a14fd0934a5e68950a044987f
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-12-17 09:12:11 +0200

    8.67. Making http requests in Angular

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -10,6 +11,19 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  baseUrl = "https://localhost:7096/api/"
+  // inject HTTP Client
+  private http = inject(HttpClient);
   title = 'Skinet';
+  products: any[] = [];
+
+  ngOnInit(): void {
+    this.http.get<any>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.info('complete')
+    })
+  }
 }
