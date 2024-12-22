0565b74fd799d64459a2c55bd149c203c195c936
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-12-22 16:35:53 +0200

    9.77. Getting the types and brands lists

--- skinet/client/src/app/core/services/shop.service.ts
+++ skinet/client/src/app/core/services/shop.service.ts
@@ -11,8 +11,32 @@ export class ShopService {
   baseUrl = "https://localhost:7096/api/"
   // inject HTTP Client
   private http = inject(HttpClient);
+  types: string[] = [];
+  brands: string[] = [];
 
   getProducts() {
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products?pageSize=20');
+  }
+
+  getBrands() {
+    // condition for not executing at infinite
+    if (this.brands.length > 0)
+      return;
+
+    return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe({
+      next: response => this.brands = response,
+      error: error => console.log(error)
+    });
+  }
+
+  getTypes() {
+    // condition for not executing at infinite
+    if (this.types.length > 0)
+      return;
+
+    return this.http.get<string[]>(this.baseUrl + 'products/types').subscribe({
+      next: response => this.types = response,
+      error: error => console.log(error)
+    });
   }
 }
