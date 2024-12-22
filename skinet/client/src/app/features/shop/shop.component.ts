1c977abc489d3770afaf8e52f82e5faa01dedaa0
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-12-22 15:57:18 +0200

    9.76. Adding a product item component

--- skinet/client/src/app/features/shop/shop.component.ts
+++ skinet/client/src/app/features/shop/shop.component.ts
@@ -2,13 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../shared/models/products';
 import { MatCard } from '@angular/material/card';
+import { ProductItemComponent } from "./product-item/product-item.component";
 
 @Component({
   selector: 'app-shop',
   standalone: true,
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
-  imports: [MatCard]
+  imports: [MatCard, ProductItemComponent]
 })
 export class ShopComponent implements OnInit {
   private shopService = inject(ShopService);
