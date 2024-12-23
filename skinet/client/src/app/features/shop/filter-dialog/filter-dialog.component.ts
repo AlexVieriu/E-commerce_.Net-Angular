0a152ef8bb6645ea34fe0f86419da5240661868a
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-12-23 10:31:40 +0200

    9.78. Adding filtering functionality using Material Dialog

---
+++ skinet/client/src/app/features/shop/filter-dialog/filter-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { ShopService } from '../../../core/services/shop.service';
+import { MatDivider } from '@angular/material/divider';
+import { MatListOption, MatSelectionList } from '@angular/material/list';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-filter-dialog',
+  standalone: true,
+  imports: [MatDivider, MatSelectionList, MatListOption, MatButton],
+  templateUrl: './filter-dialog.component.html',
+  styleUrl: './filter-dialog.component.scss'
+})
+export class FilterDialogComponent {
+  shopService = inject(ShopService);
+}
