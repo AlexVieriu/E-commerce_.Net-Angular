8bcc93a42c4430c19524c276e6a6ac9afa0ef4c4
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-12-25 11:40:27 +0200

    81. Adding the sorting functionality

--- skinet/client/src/app/features/shop/filter-dialog/filter-dialog.component.ts
+++ skinet/client/src/app/features/shop/filter-dialog/filter-dialog.component.ts
@@ -1,16 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ShopService } from '../../../core/services/shop.service';
 import { MatDivider } from '@angular/material/divider';
 import { MatListOption, MatSelectionList } from '@angular/material/list';
 import { MatButton } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-filter-dialog',
   standalone: true,
-  imports: [MatDivider, MatSelectionList, MatListOption, MatButton],
+  imports: [
+    MatDivider,
+    MatSelectionList,
+    MatListOption,
+    MatButton,
+    FormsModule],
   templateUrl: './filter-dialog.component.html',
   styleUrl: './filter-dialog.component.scss'
 })
 export class FilterDialogComponent {
   shopService = inject(ShopService);
+  private dialogRef = inject(MatDialogRef<FilterDialogComponent>);
+  data = inject(MAT_DIALOG_DATA);
+
+  selectBrands: string[] = this.data.selectBrands;
+  selectTypes: string[] = this.data.selectTypes;
+
+  applyFilters() {
+    this.dialogRef.close({
+      selectBrands: this.selectBrands,
+      selectTypes: this.selectTypes
+    })
+  }
 }
--- skinet/client/src/app/features/shop/shop.component.ts
+++ skinet/client/src/app/features/shop/shop.component.ts
@@ -3,17 +3,41 @@ import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../shared/models/products';
 import { MatCard } from '@angular/material/card';
 import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog';
+import { FilterDialogComponent } from './filter-dialog/filter-dialog.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
-  imports: [MatCard, ProductItemComponent]
+  imports: [
+    MatCard,
+    ProductItemComponent,
+    MatButton, MatIcon,
+    MatMenu,
+    MatSelectionList,
+    MatListOption,
+    MatMenuTrigger]
 })
+
 export class ShopComponent implements OnInit {
   private shopService = inject(ShopService);
+  private dialogService = inject(MatDialog);
+
   products: Product[] = [];
+  selectBrands: string[] = [];
+  selectTypes: string[] = [];
+  selectedSort: string = 'name';
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low-High', value: 'priceAsc' },
+    { name: 'Price: High-Low', value: 'priceDesc' }
+  ]
 
   ngOnInit(): void {
     this.initializeShop();
@@ -22,9 +46,45 @@ export class ShopComponent implements OnInit {
   initializeShop() {
     this.shopService.getBrands();
     this.shopService.getTypes();
-    this.shopService.getProducts().subscribe({
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.selectBrands, this.selectTypes, this.selectedSort).subscribe({
       next: response => this.products = response.data,
       error: error => console.log(error)
     })
+  }
+
+  onSortChange(event: MatSelectionListChange) {
+    // we select the first option on the list by default
+    const selectedOption = event.options[0];
+    if (selectedOption) {
+      this.selectedSort = selectedOption.value;
+      this.getProducts();
+    }
+  }
+
+  openFiltersDialog() {
+    const dialogRef = this.dialogService.open(FilterDialogComponent, {
+      width: '500px',
+      data: {
+        selectBrands: this.selectBrands,
+        selectTypes: this.selectTypes
+      }
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: result => {
+        if (result) {
+          console.log(result);
+          this.selectBrands = result.selectBrands;
+          this.selectTypes = result.selectTypes;
+
+          // apply filters
+          this.getProducts();
+        }
+      }
+    });
   }
 }
