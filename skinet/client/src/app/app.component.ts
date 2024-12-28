2f687618023c4d608171ae4fc721be3fb8c4b3d4
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-12-28 07:04:50 +0200

    10.87. Creating components and routing

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
 import { ShopComponent } from "./features/shop/shop.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HeaderComponent, ShopComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
 })
 export class AppComponent {
   title = 'Skinet';
--- skinet/client/src/app/app.routes.ts
+++ skinet/client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ShopComponent } from './features/shop/shop.component';
+import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // '' -> correspond to the route path of the app (ex: https://skinet.com/)
+    { path: '', component: HomeComponent },
+
+    // 'shop' -> https://example.com/shop
+    { path: 'shop', component: ShopComponent },
+
+    // 'shop/:id' -> https://example.com/shop/1
+    { path: 'shop/:id', component: ProductDetailsComponent },
+
+    // '**' matches any URL that hasn't been defined in the routes above
+    // redirect to HomeComponent 
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];
