0457fb21e637cd8b91c76694d275492092450836
AlexVieriu <vieriu.alexandru.25@gmail.com>
2025-01-07 11:36:46 +0200

    13.113. Creating the cart components

---
+++ skinet/client/src/app/shared/models/cart.ts
@@ -0,0 +1,9 @@
+import { CartItem } from "./cartItem";
+import { CartType } from "./cartType";
+import { nanoid } from "nanoid";
+
+export class Cart implements CartType {
+    // nanoid(10) //=> "IRFa-VaY2b"
+    id: string = nanoid(); //=> "V1StGXR8_Z5jdHi6B-myT"
+    items: CartItem[] = [];
+}
