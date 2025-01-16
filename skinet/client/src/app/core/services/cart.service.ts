f864f50ac4f0fd9cfc9113aa9b97ac8ec06bbc81
AlexVieriu <vieriu.alexandru.25@gmail.com>
2025-01-16 12:06:23 +0200

    125. Adding these functions to the cart

--- skinet/client/src/app/core/services/cart.service.ts
+++ skinet/client/src/app/core/services/cart.service.ts
@@ -46,6 +46,36 @@ export class CartService {
     });
   }
 
+  removeItemFromCart(productId: number, quantity = 1) {
+    const cart = this.cart();
+    if (!cart) return;
+
+    const index = cart.items.findIndex(i => i.productId === productId);
+    if (index != -1) {
+      if (cart.items[index].quantity > quantity) {
+        cart.items[index].quantity -= quantity;
+      }
+      else {
+        cart.items.splice(index, 1);
+      }
+      if (cart.items.length === 0) {
+        this.deleteCart();
+      }
+      else {
+        this.setCartAsync(cart)
+      }
+    }
+  }
+
+  deleteCart() {
+    this.http.delete(this.baseUrl + 'cart?id=' + this.cart()?.id).subscribe({
+      next: () => {
+        localStorage.removeItem('cart_id');
+        this.cart.set(null);
+      }
+    })
+  }
+
   addItemToCart(item: CartItem | Product, quantity = 1) {
     const cart = this.cart() ?? this.createCart();
 
