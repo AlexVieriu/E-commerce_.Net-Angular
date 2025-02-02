4849f64b7629497054785d9cd775aa723ce83064
AlexVieriu <vieriu.alexandru.25@gmail.com>
2025-02-02 07:00:33 +0200

    14.134. Creating additional user endpoints

--- skinet/API/Controllers/AccountController.cs
+++ skinet/API/Controllers/AccountController.cs
@@ -1,3 +1,6 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
 namespace API.Controllers;
 
 [Route("api/[controller]")]
@@ -21,5 +24,38 @@ public class AccountController(SignInManager<AppUser> signInManager) : Controlle
             return BadRequest(result.Errors);
 
         return Ok();
+    }
+
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return NoContent();
+    }
+
+    [HttpGet("user-info")]
+    public async Task<ActionResult> GetUserInfo()
+    {
+        if (User.Identity?.IsAuthenticated == false)
+            return NoContent();
+
+        var user = await signInManager.UserManager.Users
+            .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
+
+        if (user == null)
+            return Unauthorized();
+
+        return Ok(new
+        {
+            user.FirstName,
+            user.LastName,
+            user.Email
+        });
+    }
+
+    [HttpGet]
+    public ActionResult GetAuthState()
+    {
+        return Ok(new { IsAuthenticated = User.Identity?.IsAuthenticated });
     }
 }
