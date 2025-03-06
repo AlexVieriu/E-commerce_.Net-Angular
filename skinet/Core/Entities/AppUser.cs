12023802b86982cfadd394fabc9532aaef7da3ce
AlexV89 <vieriu.alexandru.25@gmail.com>
2025-03-06 17:14:09 +0200

    14.137. Adding a user address class

--- skinet/Core/Entities/AppUser.cs
+++ skinet/Core/Entities/AppUser.cs
@@ -4,4 +4,5 @@ public class AppUser : IdentityUser
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public Address? Address { get; set; }
 }
--- skinet/Infrastructure/Data/StoreContext.cs
+++ skinet/Infrastructure/Data/StoreContext.cs
@@ -3,6 +3,7 @@ namespace Infrastructure.Data;
 public class StoreContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
 {
     public required DbSet<Product> Products { get; set; }
+    public required DbSet<Address> Addresses { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
