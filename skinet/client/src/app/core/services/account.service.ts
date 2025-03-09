92a24930ae2a469df45250e4081de34241f43e38
AlexV89 <vieriu.alexandru.25@gmail.com>
2025-03-09 08:05:07 +0200

    142. Creating the account components

---
+++ skinet/client/src/app/core/services/account.service.ts
@@ -0,0 +1,52 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { User } from '../../shared/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
+
+  login(values: any) {
+    // build HTTP parameters (query string parameters) for the request
+    let params = new HttpParams();
+
+    // adding a parameter named 'useCookies' with the value 'true'
+    params = params.append('useCookies', true);
+
+    /*
+    post<T>(
+    url: string, 
+    body: any | null, 
+    options?: {
+      params?: HttpParams | {
+        [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>;
+        };
+      }
+    ): Observable<T>;
+    */
+    return this.http.post<User>(this.baseUrl + 'account/login', values, { params });
+  }
+
+  register(values: any) {
+    return this.http.post(this.baseUrl + 'account/register', values);
+  }
+
+  getUserInfo() {
+    return this.http.get<User>(this.baseUrl + 'account/user-info').subscribe({
+      next: user => this.currentUser.set(user)
+    })
+  }
+
+  logout() {
+    return this.http.post(this.baseUrl + 'account/logout', {});
+  }
+
+  updateAddress(address: any) {
+    return this.http.put(this.baseUrl + 'account/address', address);
+  }
+}
