fc71448997b43cabfed1f16687e7fa718a471e87
AlexV89 <vieriu.alexandru.25@gmail.com>
2025-03-09 18:34:00 +0200

    146. Persisting the login

--- skinet/client/src/app/core/services/init.service.ts
+++ skinet/client/src/app/core/services/init.service.ts
@@ -1,18 +1,20 @@
 import { inject, Injectable } from '@angular/core';
 import { CartService } from './cart.service';
-import { asyncScheduler, scheduled } from 'rxjs';
+import { asyncScheduler, forkJoin, scheduled } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InitService {
-  cartService = inject(CartService);
+  private cartService = inject(CartService);
+  accountService = inject(AccountService);
 
   init() {
     const cartId = localStorage.getItem('cart_id');
     const cart$ = cartId ? this.cartService.getCartAsync(cartId)
       : scheduled([null], asyncScheduler);
 
-    return cart$;
+    return forkJoin([cart$, this.accountService.getUserInfo()]);
   }
 }
