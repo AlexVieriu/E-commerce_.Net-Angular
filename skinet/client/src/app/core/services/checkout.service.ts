1409066593d9ab730c1a88c24a7b6a0208875801
AlexV89 <vieriu.alexandru.25@gmail.com>
2025-03-19 18:07:02 +0200

    170. Creating the delivery component part2

--- skinet/client/src/app/core/services/checkout.service.ts
+++ skinet/client/src/app/core/services/checkout.service.ts
@@ -16,8 +16,9 @@ export class CheckoutService {
   getDeliveryMethods() {
     if (this.deliveryMethods.length > 0)
       return scheduled([this.deliveryMethods], asyncScheduler);
+    // return of(this.deliveryMethods);
 
-    return this.http.get<DeliveryMethod[]>(this.baseUrl + 'orders/delivery-methods').pipe(
+    return this.http.get<DeliveryMethod[]>(this.baseUrl + 'payments/delivery-methods').pipe(
       map(methods => {
         this.deliveryMethods = methods.sort((a, b) => b.price - a.price);
         return methods;
@@ -25,3 +26,8 @@ export class CheckoutService {
     )
   }
 }
+
+/*
+The selector you specified (#address-element) applies to no DOM elements 
+that are currently on the page. Make sure the element exists on the page before calling mount().
+*/
