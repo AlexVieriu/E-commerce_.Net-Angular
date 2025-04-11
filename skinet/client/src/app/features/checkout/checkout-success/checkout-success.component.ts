3f5699bda7fb8517073c036d08b501f6cc0528ad
Vieriu Alexxandru <vieriu.alexandru.25@gmail.com>
2025-04-11 17:46:15 +0300

    206. Adding a guard to the checkout success

--- skinet/client/src/app/features/checkout/checkout-success/checkout-success.component.ts
+++ skinet/client/src/app/features/checkout/checkout-success/checkout-success.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { SignalrService } from '../../../core/services/signalr.service';
@@ -6,6 +6,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CurrencyPipe, DatePipe, NgIf } from '@angular/common';
 import { AddressPipe } from '../../../shared/pipes/address.pipe';
 import { CardPipe } from '../../../shared/pipes/card.pipe';
+import { OrderService } from '../../../core/services/order.service';
 
 @Component({
   selector: 'app-checkout-success',
@@ -21,7 +22,13 @@ import { CardPipe } from '../../../shared/pipes/card.pipe';
   templateUrl: './checkout-success.component.html',
   styleUrl: './checkout-success.component.scss'
 })
-export class CheckoutSuccessComponent {
+export class CheckoutSuccessComponent implements OnDestroy {
   signalrService = inject(SignalrService);
+  private orderService = inject(OrderService);
 
+  ngOnDestroy(): void {
+    // once they left out the checkout success page, we set the flag to false and redirect someone else
+    this.orderService.orderComplete = false;
+    this.signalrService.orderSignal.set(null);
+  }
 }
